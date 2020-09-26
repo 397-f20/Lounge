1f8da158905740fd743c87ef2d1f4d4a8e9c67e4
Drew Parsons <drewparsons@Drews-MacBook-Pro.local>
2020-09-26 14:01:15 -0600

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+import "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASZ_dPEaJz6zic_SnPBMHoFydp_PkPpa8",
+    authDomain: "lounge-abdc4.firebaseapp.com",
+    databaseURL: "https://lounge-abdc4.firebaseio.com",
+    projectId: "lounge-abdc4",
+    storageBucket: "lounge-abdc4.appspot.com",
+    messagingSenderId: "1037521076753",
+    appId: "1:1037521076753:web:7f88c6e37f973feae067b0",
+    measurementId: "G-T252V65GKY"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
