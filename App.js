b01a1499bbf03c56d8c785974ca3c5f49b0ebf71
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2020-09-27 15:29:31 -0600

    Added Native Base, some Firebase stuff

--- App.js
+++ App.js
@@ -1,29 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Header, Title, Content, Button, Icon, Left, Right, Body, Text, List, ListItem } from "native-base";
 import { firebase } from './firebase';
 
-const Lounge = () => {
+const Lobby = () => {
+  const [lobbyNames, setLobbyNames] = useState({});
+  useEffect(() => {
+    const db = firebase.database().ref('Lobby');
+    const handleData = snap => {
+      if (snap.val())  {
+        setLobbyNames(snap.val());
+        console.log(snap.val())
+        console.log('lobby names are: ')
+        console.log(lobbyNames)
+      }
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
+  
+  const dbNames = ['Grady', 'Drew', 'Joyce']
   return (
-    <Text>The lounge is currently empty!</Text>
+    <Content>
+      <List>
+        {lobbyNames ? dbNames.map(user => (
+        <ListItem key={user}>
+          <Text>{user}</Text>
+        </ListItem>)) : <Text>loading</Text>}
+      </List>
+    </Content>
   )
 }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Lounge></Lounge>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        <Header transparent>
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Lounge</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              {/* <Text>Cancel</Text> */}
+            </Button>
+          </Right>
+        </Header>
+        <Content padder>
+          <Button>
+            <Text style={styles.container}>Join Lounge</Text>
+          </Button>
+          <Lobby></Lobby>
+        </Content>
+      </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // margin: "0 auto",
   },
 });
