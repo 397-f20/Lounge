f8a6e268168db7cec20bcbcf3588f49502fc4172
Joyce Feifei <jyszhang2020@gmail.com>
2020-09-30 16:16:49 -0500

    Change Style

--- components/Lobby.js
+++ components/Lobby.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, Button } from 'react-native';
+import { ImageBackground, Button, TextInput, Text, View, StyleSheet, SafeAreaView, SectionList } from 'react-native';
 import { firebase } from '../firebase';
+import Constants from "expo-constants";
+
+const image = { uri: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_1460,h_822/at%2Fart%2Fdesign%2Fzoom-backgrounds%2FAT-zoom-background-stayhome" };
 
 const Lobby = ({user}) => {
     const [lobbyNames, setLobbyNames] = useState(null);
@@ -28,21 +31,44 @@ const Lobby = ({user}) => {
     }
 
   
-
-
     return (
-        <View>
-            <Text>{user}</Text>
-            <Button title={"Join Lobby"} onPress={addToLobby} >
-                <Text>Join Lounge</Text>
-            </Button>
-            {lobbyNames ? lobbyNames.map(user => (
-            <Text key={user.name}>{user.name}</Text>))
-            :
-            <Text>loading</Text>}
+        <View style={styles.container}>
+            {/* </View><ImageBackground source={image} style={styles.image}> */}
+            <View>
+                <Text style={styles.header}>Hello {user}</Text>
+                <Button style={styles.button} title={"Join Lobby"} onPress={addToLobby} >
+                    <Text>Join Lounge</Text>
+                </Button>
+                {lobbyNames ? lobbyNames.map(user => (
+                <Text style={styles.list} key={user.name}>{user.name}</Text>))
+                :
+                <Text>loading</Text>}                
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+      marginHorizontal: 16
+    },
+    header: {
+      fontSize: 32,
+      //backgroundColor: "#fff",
+      margin: 50
+    },
+    list: {
+        //backgroundColor: "#f9c2ff",
+        margin: 50,
+        height: 30,
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+  });
 
 export default Lobby;
 
--- components/NameForm.js
+++ components/NameForm.js
@@ -1,13 +1,56 @@
 import React, { useState } from 'react';
-import { Button, TextInput, Text, View } from 'react-native';
+import { TouchableOpacity, TextInput, Button, Text, View, StyleSheet, SafeAreaView, SectionList } from 'react-native';
+import Constants from "expo-constants";
 
-export default function NameForm({setUser}) {
+const NameForm = ({setUser}) => {
     const [nameField, setNameField] = useState('');
     return (
-        <View>
-            <Text> Enter your name </Text>
-            <TextInput value={nameField} placeholder="eg. John Doe" onChangeText={ text => setNameField(text) }/>
-            <Button title={"Submit name"}  onPress={ () => setUser(nameField) }/>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text style={styles.header}> Enter your name </Text>
+                <TextInput style={styles.textInput} value={nameField} placeholder="eg. John Doe" onChangeText={ text => setNameField(text) }/>
+                <TouchableOpacity onPress={ () => setUser(nameField) }>
+                    <View style = {{backgroundColor: 'red', alignItems: 'center', 
+                        justifyContent: 'center', borderRadius: 15, width: 200}}>
+                            <Text style = {{color: 'white'}}>Submit Name</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+      marginHorizontal: 16
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff"
+    },
+    title: {
+      fontSize: 24
+    },
+    textInput: {
+        margin: 10,
+        height: 30,
+        borderColor: '#7a42f4',
+        borderWidth: 1,
+    },
+    button: {
+        borderRadius: 15,
+        padding: 20
+    }
+  });
+
+export default  NameForm;
+
+
+  
