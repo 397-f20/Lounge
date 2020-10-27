92552cfa00fe1f3c5284160e9d02ef8c66b72e49
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2020-10-27 10:10:47 -0700

    updated Lobby test

--- __tests__/components/lobby.test.js
+++ __tests__/components/lobby.test.js
@@ -3,12 +3,24 @@ import renderer from 'react-test-renderer';
 
 import Lobby from '../../components/Lobby'
 
+const auth = {
+    uid: "83ISLPPLMKhc9JRVqcgHjgxdxIi2",
+    email: "drew@parsons.net"
+}
+const teamId = "-asdfjnasljkdfasdf"
+const teamInfo = [{
+    firstName: "Drew",
+    lastName: "Parsons",
+    status: "online",
+    voteToClose: "false"
+}]
+
 describe('<Lobby />', () => {
     it('has 1 child', () => {
-      const login = renderer.create(<Lobby/>)
+      const login = renderer.create(<Lobby auth={auth} teamID={teamId} teamInfo={teamInfo} />)
       const tree = login.toJSON();
       // console.log(tree)
       // console.log(tree.children[0].children)
-      expect(tree.children.length).toBe(1);
+      expect(tree.children.length).toBe(4);
     });
   });
