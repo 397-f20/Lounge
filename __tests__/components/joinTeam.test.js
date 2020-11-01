0590863b884d0a99a7fadacaa57679d359cbdc9f
Joyce Feifei <jyszhang2020@gmail.com>
2020-10-31 23:05:41 -0500

    joinTeam children test

--- __tests__/components/joinTeam.test.js
+++ __tests__/components/joinTeam.test.js
@@ -11,11 +11,19 @@ const teamId = "-asdfjnasljkdfasdf"
 const rount = "joinTeam"
 
 describe('<JoinTeam />', () => {
-    it('has 1 children', () => {
-      const login = renderer.create(<JoinTeam auth={auth} teamID={teamId} />)
-      const tree = login.toJSON();
-      // console.log(tree)
-      // console.log(tree.children[0].children)
-      expect(tree.children.length).toBe(1);
-    });
+  it('has 1 children', () => {
+    const joinTeam = renderer.create(<JoinTeam auth={auth} teamID={teamId} />)
+    const tree = joinTeam.toJSON();
+    
+    // JoinTeam render
+    expect(tree.children.length).toBe(1);
+    expect(tree.children[0].children.length).toBe(3);
+    
+    // 'Team ID' filed
+    expect(tree.children[0].children[0].children[0]).toBe(' Team ID ');
+    // Text input
+    // expect(tree.children[0].children[1].lenght).toBe(1);
+    // Join buttom
+    expect(tree.children[0].children[2].children[0].children[0]).toBe('Join Team');
   });
+});
