2717bb3c2fe3fd5f17c44f45f907e3a7fed0a5c1
Kevin Hou <kevinhou168@gmail.com>
2020-11-15 16:31:04 -0600

    add history formatting & linking

--- components/History.js
+++ components/History.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity, TextInput, Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, TextInput, Platform, Text, View, StyleSheet } from 'react-native';
 import { firebase } from '../firebase';
 import styles from "../assets/Styles";
 
 
-const History = ({teamId, setRoute, setIsPlaying }) => {
+const History = ({ teamId, setRoute, setIsPlaying }) => {
     const [teamHistory, setTeamHistory] = useState([]);
     var getHistoryRef = firebase.database().ref('teams/' + teamId + '/history/');
 
@@ -25,16 +25,27 @@ const History = ({teamId, setRoute, setIsPlaying }) => {
 
     return (
         <View>
-            <Text> History Here </Text>
+            <Text style={[styles.header, styles.center]}> History Here </Text>
             {teamHistory.map(game => (
                 <View>
                     <Text>{game[1].gameName}</Text>
-                    <Text>{game[1].link}</Text>
-                    <Text>Join</Text>
+                    <Text>{game[1].created}</Text>
+                    <TouchableOpacity style={[styles.button, styles.center]} onPress={() => {
+                        if (Platform.OS == 'web') {
+                            window.open('https://meet.jit.si/' + JSON.stringify(game[1].link).slice(1, -1), '_blank');
+                        }
+                        else {
+                            Linking.openURL('https://meet.jit.si/' + JSON.stringify(game[1].link).slice(1, -1))
+                        }
+                    }}>
+                        <Text style={[styles.text, styles.center, { fontWeight: "bold" }]}>
+                            💖 Join Call!
+                        </Text>
+                    </TouchableOpacity>
                 </View>
             ))}
         </View>
     )
-} 
+}
 
 export default History;
